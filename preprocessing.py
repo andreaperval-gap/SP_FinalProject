730ea228705f5ae25185d7c4e9a383758c88132d
Oriol23 <oriolgarciap@gmail.com>
2026-01-21 00:07:32 +0100

    Finished Task 3

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 """Contains all functions related to preprocessing the data. """
 
-# To run the whole preprocessing just run the last function 
+# To run the whole preprocessing just run the last function preprocess_df()
 
 import numpy as np
 import pandas as pd
@@ -133,9 +133,9 @@ def cleaning_categorical_columns(df,categorical_cols=categorical_columns):
 
     # Strip leading/trailing whitespace from all string values in these columns
     for col in categorical_cols:
-        cleaned_df[col] = df[col].astype(str).str.strip()
+        df[col] = df[col].astype(str).str.strip()
 
-    return cleaned_df
+    return df
 
 def remove_outliers(df):
     """Remove outliers from the dataset. 
@@ -154,8 +154,22 @@ def remove_outliers(df):
     no_outliers_df = df.drop(labels=idxs,inplace=False)
     return no_outliers_df
 
+def normalize_df(df, numeric_columns=numeric_columns):
+    """Normalizes the numerical columns in the df using 0-1 normalization.
+    
+    Args: 
+        df: a dataframe
+        numeric_columns: a list of the numeric column names
+    
+    Returns:
+        The dataframe with its numerical columns normalized.
+    """
 
-def clean_df(df, new_columns, numeric_columns=numeric_columns):
+    df[numeric_columns] = (df[numeric_columns]-df[numeric_columns].min()) / \
+    (df[numeric_columns].max()-df[numeric_columns].min())
+    return df
+
+def preprocess_df(df, new_columns=new_columns, numeric_columns=numeric_columns):
     """Peforms data cleaning on the dataframe. 
     
     This is the entire data cleaning pipeline. The new_columns and
@@ -164,18 +178,21 @@ def clean_df(df, new_columns, numeric_columns=numeric_columns):
     Args: 
         df: a dataframe
         new_columns: a list of the new column names.  
-        numeric_columns: a list of the column names to be converted
+        numeric_columns: a list of the numeric column names
 
     Returns: 
         The cleaned dataframe.
     """
     # Renames all the columns to clearer names
-    df = rename_columns(df,new_columns)
+    df = rename_columns(df)
     # Replaces the missing values (default "?") with np.nan
     df = missing_to_nan(df)
     # Converts the numeric columns to numeric type (default object)
-    df = columns_to_numeric(df, numeric_columns)
+    df = columns_to_numeric(df)
     # Cleans the formatting of the categories names in categorical variables
     df = cleaning_categorical_columns(df)
     # Remove outliers
     df = remove_outliers(df)
+    # Normalize numerical values 0-1
+    df = normalize_df(df)
+    return df
